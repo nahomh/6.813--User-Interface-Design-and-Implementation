54ca58c1cf9e173ff914f9b5c8b8ecbddf1d2dd5
lihaoyi <haoyi.sg@gmail.com>
2012-02-23 23:03:03 -0500

    sd

--- src/spentrax/main/SpentraxActivity.java
+++ src/spentrax/main/SpentraxActivity.java
@@ -5,8 +5,8 @@ import android.os.Bundle;
 
 public class SpentraxActivity extends Activity {
     /** Called when the activity is first created. */
+	// hello world haoyi
     @Override
-    //this works-nahoms
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);

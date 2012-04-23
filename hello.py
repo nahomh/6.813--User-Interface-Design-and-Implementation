541dadadfb270af7f809f6dc3e0b3091e2978b52
lihaoyi <haoyi.sg@gmail.com>
2012-04-22 23:37:05 -0400

    i

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from models import *
 app = Flask(__name__)
 app.debug = True
 
@@ -9,27 +10,24 @@ app.debug = True
 
 @app.route('/analytics')
 def analytics():
-    print "analytics"
     return render_template("analytics.html")
 
 @app.route('/transfer')
 def transfer():
-    print "transfer"
     return render_template("transfer.html")
- 
-
  
 @app.route('/debts')
 def debts():
-    print "debts"
     return render_template("debts.html")
-    
-
+   
 
 @app.route('/record')
 def record():
-    print "record"
     return render_template("records.html")
+    
+@app.route('/analytics')
+def record():
+    return render_template("analytics.html")
 
 
 if __name__ == '__main__':

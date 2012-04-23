a753112b20db4e3ea41598af3e19dc7f6159b0c6
lihaoyi <haoyi.sg@gmail.com>
2012-04-22 23:15:51 -0400

    i

--- hello.py
+++ hello.py
@@ -5,29 +5,23 @@ app.debug = True
 
 
 
-    
-@app.route('/')
-def hello_world():
-    return render_template("home.html")
 
-    
-@app.route('/record')
-def hello_world():
-    return render_template("records.html")
-
-@app.route('/transfer')
-def hello_world():
-    return render_template("transfer.html")
 
 @app.route('/analytics')
 def hello_world():
     return render_template("analytics.html")
+
+@app.route('/transfer')
+def hello_world():
+    return render_template("transfer.html")
     
 @app.route('/debts')
 def hello_world():
     return render_template("debts.html")
     
-
+@app.route('/record')
+def hello_world():
+    return render_template("records.html")
 
 
 if __name__ == '__main__':

60630993c7d3ac92d8009952402dc3385f147dba
lihaoyi <haoyi.sg@gmail.com>
2012-04-22 21:51:40 -0400

    rising from the ashes

---
+++ hello.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import render_template
+from stuff import *
+app = Flask(__name__)
+app.debug = True
+
+
+class Data:
+    x = "1"
+    
+@app.route('/')
+def hello_world():
+    Data.x = Data.x + "1"
+    return Data.x 
+    #return render_template("traxx.html")
+
+    
+@app.route('/home')
+def hello_world():
+    Data.x = Data.x + "1"
+    return Data.x 
+    
+if __name__ == '__main__':
+    app.run()

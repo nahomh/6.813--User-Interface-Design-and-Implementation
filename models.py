b0485bae445e3c3e5d504fab3727fca9a5c594fc
AKIRA <monchan89@gmail.com>
2012-04-25 13:24:32 -0400

    bug

--- models.py
+++ models.py
@@ -18,8 +18,8 @@ class Debt(object):
         self.amount = amount         #Number
 
 all = {0:User(),1:User(),2:User()}
-all[0].records.append(Record(40.77377,-73.983307)
+all[0].records.append(Record(40.77377,-73.983307))
 
 all[1].records.append(Record(35.685361,139.753368,[Debt(100.00,0)]))
 
-all[2].records.append(Record(35.685361,139.753368,[Debt(50.00,1),Debt(40.50,1),Debt(10.15,None,0),Debt(10.55,None,1)])
+all[2].records.append(Record(35.685361,139.753368,[Debt(50.00,1),Debt(40.50,1),Debt(10.15,None,0),Debt(10.55,None,1)]))

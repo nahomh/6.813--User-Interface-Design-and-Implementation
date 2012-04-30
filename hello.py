067db044f1d01f1a55715a799c8be5945c6af710
unknown <NahomH@.(none)>
2012-04-29 23:22:54 -0400

    added adddebts page

--- hello.py
+++ hello.py
@@ -20,6 +20,8 @@ def root_route():
 def transfer_route():
 	return render_template("transfer.html", my_ex_types=users[myUserId].ex_types, today=datetime.now())
 
+	
+	
 @app.route('/debts')
 def debts_route():
 
@@ -84,7 +86,14 @@ def debt_records_route(id=None):
 				us_rec.append(i)
 		
 	return render_template("invdebt.html",urec = us_rec, my_records=my_records)
+
+@app.route('/addDebts/')
+@app.route('/addDebts/<id>')
+def add_debts_oute(id=None):
+    debt = Debt() if id == None else debts[int(id)]
+    return render_template("addDebts.html", debt=debt)	
 	
+
 @app.route("/data-test")
 def datatest_route():
 	output = "<html><head><title>Data Test</title></head><body>"
